6c92877c2c6ef3b471c4a450e9fd818cec99ef3d
NotUrYogs <134258456+YogaRmd@users.noreply.github.com>
2024-10-10 09:50:14 +0700

    Add files via upload

---
+++ Praktek 2/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  Lingkaran();
+}
+
+function Lingkaran () {
+  fill(255,255,0);
+  strokeWeight(5);
+  circle(200,200,100);
+}
